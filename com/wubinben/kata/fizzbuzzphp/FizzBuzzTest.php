5a40d57c09f5ba8615a111abd116ec0950bcb0ba
Ben Wu <wubin28@gmail.com>
2014-02-12 19:44:22 +0800

    Added test class FizzBuzzTest and made its directory structure reflect to its namespace using the move of refactoring.

---
+++ com/wubinben/kata/fizzbuzzphp/FizzBuzzTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ben
+ * Date: 14-2-12
+ * Time: 下午7:38
+ */
+
+namespace com\wubinben\kata\fizzbuzzphp;
+
+
+class FizzBuzzTest extends \PHPUnit_Framework_TestCase {
+
+}
+ 
